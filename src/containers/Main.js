3acc673e549fb003025c6c4ec870e34a82cb5f35
junszhao <jun.zhao.ox@gmail.com>
2025-06-19 09:23:15 +0100

    new study

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -2,20 +2,10 @@ import React, {useEffect, useState} from "react";
 import Header from "../components/header/Header";
 import Greeting from "./greeting/Greeting";
 import Skills from "./skills/Skills";
-import StackProgress from "./skillProgress/skillProgress";
-import WorkExperience from "./workExperience/WorkExperience";
-import Projects from "./projects/Projects";
-import StartupProject from "./StartupProjects/StartupProject";
-import Achievement from "./achievement/Achievement";
 import Blogs from "./blogs/Blogs";
-import Footer from "../components/footer/Footer";
-import Talks from "./talks/Talks";
-import Podcast from "./podcast/Podcast";
-import Education from "./education/Education";
-import CaseStudies from "./caseStudies/CaseStudies";
+// import Footer from "../components/footer/Footer";
+// import CaseStudies from "./caseStudies/CaseStudies";
 import ScrollToTopButton from "./topbutton/Top";
-import Twitter from "./twitter-embed/twitter";
-import Profile from "./profile/Profile";
 import SplashScreen from "./splashScreen/SplashScreen";
 import {splashScreen} from "../portfolio";
 import {StyleProvider} from "../contexts/StyleContext";
@@ -53,20 +43,8 @@ const Main = () => {
           <>
             <Header />
             <Greeting />
-            <Skills />
-            <StackProgress />
-            <Education />
-            <WorkExperience />
-            <CaseStudies />
-            <Projects />
-            <StartupProject />
-            <Achievement />
             <Blogs />
-            <Talks />
-            <Twitter />
-            <Podcast />
-            <Profile />
-            <Footer />
+            <Skills />            
             <ScrollToTopButton />
           </>
         )}
--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -2,9 +2,8 @@ import React, {useContext} from "react";
 import {Fade} from "react-reveal";
 import emoji from "react-easy-emoji";
 import "./Greeting.scss";
-import landingPerson from "../../assets/lottie/landingPerson";
+import landingPerson from "../../assets/lottie/MainScene";
 import DisplayLottie from "../../components/displayLottie/DisplayLottie";
-import SocialMedia from "../../components/socialMedia/SocialMedia";
 import Button from "../../components/button/Button";
 import {illustration, greeting} from "../../portfolio";
 import StyleContext from "../../contexts/StyleContext";
@@ -36,19 +35,19 @@ export default function Greeting() {
               >
                 {greeting.subTitle}
               </p>
-              <SocialMedia />
+
               <div className="button-greeting-div">
-                <Button text="Contact us" href="#contact" />
+                
                 {greeting.resumeLink && (
                   <a
-                    href={require("./resume.pdf")}
-                    download="Resume.pdf"
+                    href={require("./wp.pdf")}
+                    download="WhitePaper.pdf"
                     className="download-link-button"
                   >
-                    <Button text="Download my resume" />
+                    <Button text="Download our white paper" />
                   </a>
                 )}
-              </div>
+              </div>              
             </div>
           </div>
           <div className="greeting-image-div">
