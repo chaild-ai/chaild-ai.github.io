38537b464abc680ccb971ffe8a4ff679125ff809
Vidminas <5411598+Vidminas@users.noreply.github.com>
2026-07-26 19:38:54 +0100

    Add CAS KCL conference post

--- src/portfolio.js
+++ src/portfolio.js
@@ -92,6 +92,7 @@ const blogSection = {
   display: true, // Set false to hide this section, defaults to true
   // Blog slugs: metadata is loaded from markdown frontmatter in /public/content/blog/<slug>.md
   blogSlugs: [
+    "kcl-teachers-conference",
     "festival-of-computing",
     "aha-benchmarks-report",
     "aha-benchmarks-workshop",
